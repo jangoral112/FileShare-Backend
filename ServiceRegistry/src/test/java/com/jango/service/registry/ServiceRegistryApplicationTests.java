0088c2c2b8ca1123ca124215e43798e503f71fbd
Jan Góral <jan.goral112@gmail.com>
2021-03-23 15:24:39 +0100

    Added ServiceRegistryApplicationTests to git

---
+++ ServiceRegistry/src/test/java/com/jango/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jango.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class ServiceRegistryApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
