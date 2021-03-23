9b34e5ed136795b7acfb5193fb8e2c5ae1899014
Jan Góral <jan.goral112@gmail.com>
2021-03-23 11:28:27 +0100

    Initial commit

---
+++ GatewayService/src/test/java/com/jango/gateway/service/GatewayServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jango.gateway.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class GatewayServiceApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
